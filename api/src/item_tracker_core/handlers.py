d80872ac4db1faeccb31e26600e527f8a51e97e6
ckt <wirelessatari@gmail.com>
2021-01-07 17:25:29 -0600

    Successfully connecting to the Heroku postgresql

--- api/src/item_tracker_core/handlers.py
+++ api/src/item_tracker_core/handlers.py
@@ -1,50 +1,25 @@
-from os import environ
-from uuid import uuid4
-from item_tracker_core import connexion_app, DB
+import os 
+import psycopg2
+
+#$Env:DATABASE_URL = $(heroku config:get DATABASE_URL -a found-your-stuff-api);  py.exe handlers.py
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = None
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    print(db_version)
+    cur.close()
+except Exception as error:
+    print('Cause: {}'.format(error))
+
+finally:
+    if conn is not None:
+        conn.close()
+        print('Closed DB')
 
 
-def getUserData(itemTag):
-    tagTable = DB.Table(environ['TAG_TABLE'])
-    userTable = DB.Table(environ['USER_TABLE'])
-    tagRecord = tagTable.getItem(
-        Key={
-            'item_tag': itemTag 
-        },
-        AttributesToGet=[
-        'user_id',
-        'item_desc',
-        'item_name'
-        ]
-    )
-    userId = tagRecord['Item']['user_id']
-    userRecord = userTable.getItem(
-        Key={
-            'user_id': userId 
-        },
-        AttributesToGet=[
-        'phone',
-        'email',
-        'name'
-        ]
-    )
-    return {'user_record': userRecord['Item'], 'tag_record': tagRecord['Item']}
-
-
-
-def addTag(body):
-    tagTable = DB.Table(environ['TAG_TABLE'])
-    itemTag = str(uuid4())
-    newTag = {
-        'item_tag': itemTag,
-        'user_id': 'bill',
-        'item_name': body['item_name'],
-        'item_desc': body['item_desc']
-    }
-
-    tagTable.putItem(Item=newTag)
-    
-    return {
-        'item_tag': itemTag
-    }
-
-connexion_app.add_api('openapi.yml')
