406f030f581dceaf9f02e262f8b3d2b62e74136f
ckt <wirelessatari@gmail.com>
2021-01-25 19:28:36 -0600

    remove old junky code, createTables via function

--- api/src/item_tracker_core/handlers.py
+++ api/src/item_tracker_core/handlers.py
@@ -40,47 +40,7 @@ class Message(Base):
     picture = Column('picture', LargeBinary)
     read = Column('read', Boolean, nullable=False)
 
-# meta = MetaData()
-# users = Table('users', meta,
-#                     Column('id', Integer, primary_key=True),
-#                     Column('email', String, nullable=False),
-#                     Column('password', String, nullable=False),
-#                     Column('name', String),
-#                     Column('phone_number', BigInteger),
-#                     Column('contact', Boolean, nullable=False),
-#                     Column('active', Boolean, nullable=False))
+def createTables():
+    Base.metadata.create_all(db)
 
-# tags = Table('tags', meta,
-#                     Column('id', Integer, primary_key=True),
-#                     Column('user_id',Integer, ForeignKey("users.id"), nullable=False),
-#                     Column('name', String),
-#                     Column('picture', LargeBinary)
-#                     Column('active', Boolean, nullable=False))
-
-# messages = Table('messages', meta,
-#                     Column('id', BigInteger, primary_key=True),
-#                     Column('tag_id',Integer, ForeignKey("tags.id"), nullable=False),
-#                     Column('time_stamp', DateTime, nullable=False),
-#                     Column('body', String, nullable=False),
-#                     Column('picture', LargeBinary),
-#                     Column('read', Boolean, nullable=False))
-
-# meta.create_all(db)
-# db.execute("INSERT INTO users (email, password, phone_number, contact, name, active) VALUES ('abc@gmail.com', 'abc', 1234, true, 'salvador dali', false)")
-
-# conn = None
-# try:
-#     conn = psycopg2.connect(DATABASE_URL)
-#     cur = conn.cursor()
-#     print('PostgreSQL database version:')
-#     cur.execute('SELECT version()')
-#     db_version = cur.fetchone()
-#     print(db_version)
-#     cur.close()
-# except Exception as error:
-#     print('Cause: {}'.format(error))
-
-# finally:
-#     if conn is not None:
-#         conn.close()
-#         print('Closed DB')
+createTables()
