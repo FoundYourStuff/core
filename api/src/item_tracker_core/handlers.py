8f2be081b2a0d9b817c32798a0b56f5641d03301
ckt <wirelessatari@gmail.com>
2021-01-31 17:10:13 -0600

    Potentially working API call, first for the app

--- api/src/item_tracker_core/handlers.py
+++ api/src/item_tracker_core/handlers.py
@@ -1,8 +1,11 @@
 import os
 import psycopg2
+import uuid
+import connexion
 from sqlalchemy import create_engine, Table, Column, String, MetaData, Integer, Boolean, Sequence, BigInteger, LargeBinary, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.postgresql import UUID
 
 
 #$Env:DATABASE_URL = $(heroku config:get DATABASE_URL -a found-your-stuff-api);  py.exe handlers.py
@@ -14,6 +17,8 @@ db = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=db)
 session = Session()
 Base = declarative_base()
+app = connexion.App(__name__, specification_dir='./')
+
 
 class User(Base):
     __tablename__='users'
@@ -21,14 +26,15 @@ class User(Base):
     email = Column('email', String, nullable=False)
     password = Column('password', String, nullable=False)
     name = Column('name', String)
-    phoneNumber = Column('phone_number', BigInteger)
+    phone_number = Column('phone_number', BigInteger)
     contact = Column('contact', Boolean, nullable=False)
     active = Column('active', Boolean, nullable=False)
 
 class Tag(Base):
     __tablename__="tags"
     id = Column('id', Integer, primary_key=True)
-    userID = Column('user_id',Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column('user_id',Integer, ForeignKey("users.id"), nullable=False)
+    external_id = Column('external_id', UUID, nullable=False, default=uuid.uuid4, unique=True)
     name = Column('name', String)
     picture = Column('picture', LargeBinary)
     active = Column('active', Boolean, nullable=False)
@@ -36,8 +42,8 @@ class Tag(Base):
 class Message(Base):
     __tablename__="messages"
     id = Column('id', BigInteger, primary_key=True)
-    tagID = Column('tag_id',Integer, ForeignKey("tags.id"), nullable=False)
-    timeStamp = Column('time_stamp', DateTime, nullable=False)
+    tag_id = Column('tag_id',Integer, ForeignKey("tags.id"), nullable=False)
+    time_stamp = Column('time_stamp', DateTime, nullable=False)
     body = Column('body', String, nullable=False)
     picture = Column('picture', LargeBinary)
     read = Column('read', Boolean, nullable=False)
@@ -45,13 +51,16 @@ class Message(Base):
 def createTables():
     Base.metadata.create_all(db)
 
-createTables()
 
 
-def getUserByTagId(tagId):
-    currentTag = session.query(Tag).get(tagId)
-    user = session.query(User).get(currentTag.userID)
+def getUserByExternalID(external_id):
+    currentTag = session.query(Tag).get(external_id)
+    user = session.query(User).get(currentTag.user_id)
     print(user.name)
+    return {"phone":user.phone_number,
+            "email":user.email}
 
-
-getUserByTagId(1)
+app.add_api('openapi.yml')
+app.run(port=8080)
+#createTables()
+# getUserByTagId(1)
