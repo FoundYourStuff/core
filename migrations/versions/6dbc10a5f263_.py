be42dad2f74efd9a18befa43a61d1acc2796ccd3
Arsh Chauhan <achauhan@alaska.edu>
2021-02-19 17:25:20 -0500

    Feat: Add Databse Migrations
    Uses Alembic to manage migrations. Also adds the first migration.

---
+++ migrations/versions/6dbc10a5f263_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6dbc10a5f263
+Revises: 
+Create Date: 2021-02-19 15:43:32.252007+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6dbc10a5f263'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
