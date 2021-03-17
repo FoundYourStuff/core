2b88a4337417c90ca7c45aa9b64884ba788e87d7
ckt <wirelessatari@gmail.com>
2021-03-17 18:35:42 -0500

    Properly handle NoResultsFound, close session when appropriate

--- api/handlers.py
+++ api/handlers.py
@@ -2,6 +2,7 @@ import os
 
 import connexion
 from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 
 from models import User,Tag
@@ -11,15 +12,22 @@ if not os.getenv('DATABASE_URL'):
 DATABASE_URL = os.getenv('DATABASE_URL')
 db = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=db)
-session = Session()
 
 def getUserByExternalID(external_id):
+    session = Session()
     currentTag = session.query(Tag).filter(Tag.external_id==external_id).first()
-    user = session.query(User).get(currentTag.user_id)
-    return {"phone":user.phone_number,
-            "email":user.email}
+    if currentTag is not None:
+        user = session.query(User).filter(currentTag.user_id==User.id).first()
+    session.close()
+    if currentTag is not None and user is not None:
+        return {"phone":user.phone_number,"email":user.email}
+    else: 
+        return {}, 404
+
+    
 
 def createNewUser(body):
+    session = Session()
     user = User(email=body['email'],
                     name=body['name'],
                     password=body['password'],
@@ -27,7 +35,11 @@ def createNewUser(body):
                     active=body['active'],
                     contact=body['contact'])
     session.add(user)
-    session.commit()
+    try:
+        session.commit()
+    except:
+        session.rollback()
+    #TODO: unit tests to make sure this is working properly
     return {"id":user.id,
             "email":user.email,
             "name":user.name,
@@ -36,20 +48,30 @@ def createNewUser(body):
             "active":user.active}
 
 def getUserByGuid(user_guid):
+    session = Session()
     user = session.query(User).filter(User.id==user_guid).first()
-    return {"id":user.id,
-            "email":user.email,
-            "name":user.name,
-            "phone_number":user.phone_number,
-            "contact":user.contact,
-            "active":user.active}
+    session.close()
+    if user is not None:
+        return {"id":user.id,
+                "email":user.email,
+                "name":user.name,
+                "phone_number":user.phone_number,
+                "contact":user.contact,
+                "active":user.active}
+    else:
+        return {}, 404
 
 def updateUserByGuid(body, user_guid):
+    session = Session()
     user = session.query(User).filter(User.id==user_guid).first()
     #dangerous?
     for attribute in body:
         setattr(user, attribute, body[attribute])
-    session.commit()
+    try:
+        session.commit()
+    except:
+        session.rollback()
+    #TODO: unit tests to make sure this is working properly
     return {"id":user.id,
             "email":user.email,
             "name":user.name,
@@ -58,30 +80,45 @@ def updateUserByGuid(body, user_guid):
             "active":user.active}
 
 def createNewTag(body, user_guid):
+    session = Session()
     tag = Tag(user_id=user_guid,
                 name=body['name'],
                 picture=body['picture'],
                 active=body['active'])
     session.add(tag)
-    session.commit()
+    try:
+        session.commit()
+    except:
+        session.rollback()
+    #TODO: unit tests to make sure this is working properly
     return {"id":tag.id,
             "external_id":tag.external_id,
             "name":tag.name,
             "picture":tag.picture}
 
 def getTagByGuid(tag_guid):
+    session = Session()
     tag = session.query(Tag).filter(Tag.id==tag_guid).first()
-    return {"id":tag.id,
-            "external_id":tag.external_id,
-            "name":tag.name,
-            "picture":tag.picture}
+    session.close()
+    if tag is not None:
+        return {"id":tag.id,
+                "external_id":tag.external_id,
+                "name":tag.name,
+                "picture":tag.picture}
+    else:
+        return {}, 404
 
 def updateTagByGuid(body, tag_guid):
+    session = Session()
     tag = session.query(Tag).filter(Tag.id==tag_guid).first()
     #dangerous?
     for attribute in body:
         setattr(tag, attribute, body[attribute])
-    session.commit()
+    try:
+        session.commit()
+    except:
+        session.rollback()
+    #TODO: unit tests to make sure this is working properly
     return {"id":tag.id,
             "external_id":tag.external_id,
             "name":tag.name,
@@ -89,8 +126,13 @@ def updateTagByGuid(body, tag_guid):
     
 
 def getAllUsersTags(user_guid):
+    session = Session()
     tags = session.query(Tag).filter(Tag.user_id==user_guid).all()
-    listOfTags = []
-    for tag in tags:
-         listOfTags.append({"id":tag.id,"external_id":tag.external_id,"name":tag.name,"picture":tag.picture})
-    return listOfTags
+    session.close()
+    if tags is not None:
+        listOfTags = []
+        for tag in tags:
+            listOfTags.append({"id":tag.id,"external_id":tag.external_id,"name":tag.name,"picture":tag.picture})
+        return listOfTags
+    else:
+        return {}, 404
